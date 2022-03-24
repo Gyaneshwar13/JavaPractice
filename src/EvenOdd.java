ddcecabb3d5690aae891761d390a33cf2cf9cda9
Gyaneshwar Mishra <gyaneshwarmishra13@gmail.com>
2022-03-24 15:27:24 +0530

    [Learning]:: Added EvenOdd program

---
+++ src/EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+        int a = 50 ;
+
+        if ( a % 2 == 0){
+            System.out.println("it is an even number ");
+        }
+        else {
+            System.out.println ("it is an odd number");
+        }
+
+
+    }
+}
