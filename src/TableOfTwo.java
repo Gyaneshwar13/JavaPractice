8fcc5679f7071449222dbc5f259faf13a3adb452
Gyaneshwar Mishra <gyaneshwarmishra13@gmail.com>
2022-04-09 23:36:36 +0530

    [Learning] :: Added table of two program

---
+++ src/TableOfTwo.java
@@ -0,0 +1,12 @@
+public class TableOfTwo {
+    public static void main(String[] args) {
+        int a = 2 ;
+        int table = 1;
+        for ( int i = 1 ; i <= 10 ; i++)
+        {
+            table =   a * i ;
+            System.out.println( a +"* "+ i +"= " + table);
+
+        }
+    }
+}
