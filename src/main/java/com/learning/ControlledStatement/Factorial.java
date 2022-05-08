9fa57a0380c1de88ac674c80df8922be688ee9cd
Gyaneshwar13 <gyaneshwarmishra13@gmail.com>
2022-05-08 22:54:15 +0530

    [Learning] :: adding factorial program

---
+++ src/main/java/com/learning/ControlledStatement/Factorial.java
@@ -0,0 +1,16 @@
+package com.learning.ControlledStatement;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        int a = 5 ;
+        int facto = 1;
+         for ( int i = 1 ;  i <= 5 ; i ++){
+             facto = facto * i;
+
+         }
+         System.out.println (facto);
+
+
+    }
+}
