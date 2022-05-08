778899b93e2d8e4fd1dba61f4e4496aa8ce0a8ba
Gyaneshwar13 <gyaneshwarmishra13@gmail.com>
2022-05-08 22:59:03 +0530

    [Learning] :: Adding a star program

---
+++ src/main/java/com/learning/ControlledStatement/StarPrograms/StarProgramOne.java
@@ -0,0 +1,17 @@
+package com.learning.ControlledStatement.StarPrograms;
+
+public class StarProgramOne {
+    public static void main(String[] args)
+    {
+        for (int i = 1 ; i<=4 ; i ++)
+        {
+            for ( int j = 1 ; j <= i ; j++)
+            {
+                System.out.print("*");
+            }
+                System.out.println();
+        }
+
+
+    }
+}
